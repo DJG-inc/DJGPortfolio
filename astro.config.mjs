5634d3f7ea5c44da10c5ae71d84acba2b0039271
SrOscuroBlck <camargogustavoa@gmail.com>
2024-09-11 01:03:44 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,10 @@ import netlify from "@astrojs/netlify/static"; // Updated to use Netlify
 
 export default defineConfig({
   site: "https://djg.netlify.app",
+  image: {
+    domains: ["images.unsplash.com"],
+    formats: ["avif", "webp", "jpg", "png"],
+  },
   prefetch: true,
   integrations: [
     tailwind(),
